5ba84fb0be5809b8b6fa1fe066e5e2197b7e356e
Nikita Kuznetsov <kuzn5298@gmail.com>
2024-10-22 16:17:24 +0200

    🧪 Add test configuration

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '.+\\.(css|style|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/file-mock.ts',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+};
